5e6fec01e83289b1af9c49022a6f47a2932ac3a4
Dennis Smuczynski <dennis_smuczynski@goodyear.com>
2024-05-14 10:08:07 +0200

    readded arm controls

---
+++ dobotControls/homing.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
+from lib.interface import Interface
+
+bot = Interface("COM3")
+
+print('Bot status:', 'connected' if bot.connected() else 'not connected')
+
+
+print(bot.get_device_name())
